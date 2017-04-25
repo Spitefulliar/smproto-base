04e3de6b206c2c01fcc4c81473c0188fdded02d0
Natalia Chigryay <spiteful.liar@gmail.com>
2017-04-25 19:01:13 +0300

    header menu upd

--- local/templates/.default/js/app/providers/themeProvider.js
+++ local/templates/.default/js/app/providers/themeProvider.js
@@ -1,33 +1,29 @@
 var MODULE_CONFIG = ['$mdThemingProvider', function($mdThemingProvider) {
 
-    // var customPurpleMap = $mdThemingProvider.extendPalette('deep-purple', {
-    //   '50': 'ffffff',
-    //   'A100': '9f62c9',
-    //   'A200': '663399',
-    //   'A700': '824ab0',
-    //   'contrastDefaultColor': 'light',
-    //   'contrastDarkColors': ['50'],
-    //   'contrastLightColors': ['A100', 'A200'],
-    // });
+    var customLightBlueMap = $mdThemingProvider.extendPalette('light-blue', {
+      '50': 'ffffff',
+      // 'A100': '9f62c9',
+      // 'A200': '663399',
+      // 'A700': '824ab0',
+      'contrastDefaultColor': 'light',
+      'contrastDarkColors': ['50'],
+      'contrastLightColors': ['100', '200'],
+    });
 
-    // $mdThemingProvider.definePalette('customPurple', customPurpleMap);
+    $mdThemingProvider.definePalette('customLightBlue', customLightBlueMap);
 
     $mdThemingProvider
       .theme('default')
         .dark()
-        .primaryPalette('teal')
+        .primaryPalette('customLightBlue')
         .accentPalette('blue')
         .backgroundPalette('blue-grey');
     $mdThemingProvider
         .enableBrowserColor({
           theme: 'default', // Default is 'default'
-          palette: 'accent', // Default is 'primary', any basic material palette and extended palettes are available
+          palette: 'primary', // Default is 'primary', any basic material palette and extended palettes are available
           hue: '800' // Default is '800'
         });
-
-        // .primaryPalette('blue')
-        // .accentPalette('light-blue')
-        // .backgroundPalette('teal');
   }];
 
 export default MODULE_CONFIG;
