0c304123c6a8e657330517e371bd9e9be66e7e8e
Natalia Chigryay <spiteful.liar@gmail.com>
2017-05-03 15:55:31 +0300

    banners grid

---
+++ local/templates/.default/js/app/components/bannersGrid/directive.js
@@ -0,0 +1,52 @@
+//DIRECTIVE
+import MODULE_CONFIG from './config';
+
+export default ['$rootScope','$http', '$timeout', '$window', '$state',
+  function ($rootScope, $http, $timeout, $window, $state) {
+    var linkFunction = function linkFunction($scope, $element, $attributes) {
+
+      console.log($scope);
+
+      /**
+       * function for assigning cell sizes to array of banners
+       *
+       * @param {array} [banners=[]]
+       * @param {number} [baseCellHeight=1]
+       * @returns {array} banners
+       */
+      function makeGridPattern(banners = [], baseCellHeight = 1) {
+
+        let sizes = [ [2,1], [1,2], [2,1], [1,2] ];
+
+        while(sizes.length < banners.length) {
+          sizes.concat(sizes);
+        }
+
+        banners.map(function(item, i) {
+          console.log(sizes[i]);
+          return Object.assign(item, {
+            "rowspan": sizes[i][0] * baseCellHeight,
+            "colspan": sizes[i][1]
+          });
+        });
+        console.log(banners);
+        return banners;
+      }
+
+      // $rootScope.$on('$pageDataLoaded', function(event) {
+        $scope.banners = makeGridPattern($scope.banners, $scope.baseCellHeight);
+      // });
+    };
+
+  return {
+    restrict: "AE",
+    link: linkFunction,
+    controller: MODULE_CONFIG.CONTROLLER_NAME,
+    scope: {
+      banners: '=',
+      baseCellHeight: '@',
+      blockType: '@'
+    },
+    template: require('./template.html')
+  };
+}];
