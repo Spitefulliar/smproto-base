2dc155e86ddf3cac46ab893d3d44bc208c7eb441
Natalia Chigryay <spiteful.liar@gmail.com>
2017-05-04 15:20:28 +0300

    mailing subscription popup done

--- local/templates/.default/js/app/components/mailingSubs/controller.js
+++ local/templates/.default/js/app/components/mailingSubs/controller.js
@@ -28,8 +28,6 @@ export default ['$scope', '$rootScope', MODULE_CONFIG.SERVICE_NAME, '$log', '$ti
     }
     var config = {
       attachTo: attachment,
-      // controller: ctrl,
-      // controllerAs: 'popupImgCtrl',
       template: require('./template.html'),
       panelClass: 'mailing-subs-popup',
       position: position,
@@ -43,40 +41,18 @@ export default ['$scope', '$rootScope', MODULE_CONFIG.SERVICE_NAME, '$log', '$ti
       focusOnOpen: true,
       zIndex: zindex,
       disableParentScroll: true,
-      onDomAdded: function() {
-        $scope.popupRendered = true;
-        // let wrapper = $('.popup-img').parents('.md-panel-outer-wrapper');
-        // wrapper.append($('.popup-img .popup-img__close_outer'));
-
-        // let scrollHandler = function(event) {
-        //   event.preventDefault();
-        //   popupImgCtrl.prototype.closePanel();
-        // };
-
-        // $(window).on('scroll', scrollHandler);
-      },
-      onDomRemoved: function() {
-        $scope.popupRendered = false;
-        // $(window).off('scroll', scrollHandler);
-      }
     };
 
     panelRef = $scope._mdPanel.create(config);
     panelRef.open();
 
-    // if (!dontCLoseOnResize) {
-    //   $(window).resize(function(){
-    //     panelRef.close();
-    //   })
-    // }
-
-    ctrl.closeThisPanel = this.closePanel = function() {
+    $scope.close = this.closePanel = function() {
       panelRef.close();
     }
 
     $rootScope.$on('$stateChangeStart',
     function(event, toState, toParams, fromState, fromParams){
-      ctrl.closeThisPanel();
+      $scope.close();
     });
 
     return true;
--- local/templates/.default/js/app/components/mailingSubs/directive.js
+++ local/templates/.default/js/app/components/mailingSubs/directive.js
@@ -4,7 +4,66 @@ import MODULE_CONFIG from './config';
 export default ['$rootScope','$http', '$timeout', '$window', '$state',
   function ($rootScope, $http, $timeout, $window, $state) {
     var linkFunction = function linkFunction($scope, $element, $attributes) {
+        $scope.mailingPopupData = {
+          "title": "За подписку на новости &quot;БРЕНДА&quot; <br>дарим скидку 5% на все товары",
+          "description": "Звездочкой отмечены обязательные <br>для заполнения поля",
+          "fields": {
+              "name": {
+                "label": "Name",
+                "error": {
+                  "required": "fieldError.required"
+                }
+              },
+              "email": {
+                "label": "Email address",
+                "error": {
+                  "required": "fieldError.required"
+                }
+              },
+              "phone": {
+                "label": "Phone",
+                "error": {
+                  "minlength": "fieldError.phone.minlength",
+                  "maxlength": "fieldError.phone.maxlength"
+                }
+              },
+              "sex": {
+                "label": "Sex",
+                "options": [
+                  {
+                    "label": "Male",
+                    "value":"male"
+                  },
+                  {
+                    "label": "Female",
+                    "value":"female"
+                  }
+                ]
+              },
+              "dateOfBirth": {
+                "label": "Date of birth",
+                "fields": {
+                  "day": {
+                    "label": "Day"
+                  },
+                  "month": {
+                    "label": "Month"
+                  },
+                  "year": {
+                    "label": "Year"
+                  },
+                }
+              },
+              "policy": {
+                "label": "Согласен с <a href='#'>политикой конфиденциальности<sup>*</sup></a>"
+              }
+            },
+          "btnText": "Subscribe"
+        }
 
+        $scope.submit = function() {
+          $scope.close();
+        }
     };
   return {
     restrict: "AE",
